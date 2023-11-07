e20050abfbc1f5fcb8a7af831fc45b8fb7b598b2
Edimilson Borges <104403198+EdimilsonBorges@users.noreply.github.com>
2023-11-07 19:05:47 -0300

    feat: implementação das cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -16,13 +16,17 @@ const state = {
         computer: document.getElementById("computer-infield-cards"),
     },
 
-    actions:{
-        button:document.getElementById("next-duel"),
+    actions: {
+        button: document.getElementById("next-duel"),
     }
 
 };
 
-const pathImage = ".src/assets/icons/";
+const playerSide = {
+    player1: "player-cards",
+    computer: "computer-cards",
+}
+const pathImage = "./src/assets/icons/";
 const cardData = [
     {
         id: 0,
@@ -50,7 +54,48 @@ const cardData = [
     },
 ];
 
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function drawSelectCard(idCard){
+    state.cardSprites.avatar.src = cardData[idCard].img;
+    state.cardSprites.name.innerText = cardData[idCard].name;
+    state.cardSprites.type.innerText = `Attribute: ${cardData[idCard].type}`;
+}
+
+async function createCardImage(idCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", idCard);
+    cardImage.classList.add("card");
+
+    if (fieldSide === playerSide.player1) {
+        cardImage.addEventListener("click", () => {
+            setCardField(cardImage.getAttribute("data-id"));
+        });
+
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(idCard);
+        });
+    }
+
+    return cardImage;
+}
+
+async function drawCards(cardNumber, fieldSide) {
+    for (let i = 0; i < cardNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
 function init() {
+    drawCards(5, playerSide.player1);
+    drawCards(5, playerSide.computer);
 }
 
 init();
